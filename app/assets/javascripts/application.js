1527e8e25c4d9ba953fe7995119aae7df04f55e1
Chandler Kelley <chandler.t.kelley@gmail.com>
2016-09-29 17:48:28 -0400

    Added page jump easing and project hover zoom animation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,5 +17,16 @@
 //= require turbolinks
 
 $(document).ready(function() {
-	// Use .ready to ensure turbolinks works properly
-})
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
