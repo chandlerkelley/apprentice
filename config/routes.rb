17b616f7bd6a14cad5a30cef03bd6641ba7fef35
Chandler Kelley <chandler.t.kelley@gmail.com>
2016-09-30 10:16:31 -0400

    Bootstrapped Google Maps

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root 'home#index'
   
   get 'instructions', to: 'home#instructions', as: 'instructions'
+  get 'map', to: 'home#map', as: 'map'
 
   devise_for :users
   
